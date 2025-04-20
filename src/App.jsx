7ce27b46633e41b6401a511def5cc29ff19998bb
Rui Fernandes <rui95fer@gmail.com>
2025-04-20 18:45:33 +0100

    Completing the Static UI Migration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { PageWrapper } from "./components/PageWrapper.jsx";
 import { Container } from "./components/Container.jsx";
 import { Header } from "./components/Header.jsx";
+import { Search } from "./components/Search.jsx";
+import { Shortlist } from "./components/Shortlist.jsx";
+import { PuppiesList } from "./components/PuppiesList.jsx";
+import { NewPuppyForm } from "./components/NewPuppyForm.jsx";
 
 export function App() {
   return (
     <PageWrapper>
       <Container>
         <Header />
+        <Main />
       </Container>
     </PageWrapper>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <div className="mt-24 grid gap-8 sm:grid-cols-2">
+        <Search />
+        <Shortlist />
+      </div>
+      <PuppiesList />
+      <NewPuppyForm />
+    </main>
   );
 }
