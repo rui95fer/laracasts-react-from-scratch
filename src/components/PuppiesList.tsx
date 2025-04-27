00d26d6cca0defdc5e80656084e071e48b97654a
Rui Fernandes <rui95fer@gmail.com>
2025-04-27 17:17:41 +0100

    Controlled Vs Uncontrolled Inputs

--- src/components/PuppiesList.tsx
+++ src/components/PuppiesList.tsx
@@ -3,19 +3,30 @@ import { LikeToggle } from "./LikeToggle";
 import { Dispatch, SetStateAction } from "react";
 
 export function PuppiesList({
+  searchQuery,
   puppies,
   liked,
   setLiked,
 }: {
+  searchQuery: string;
   puppies: Puppy[];
   liked: Puppy["id"][];
   setLiked: Dispatch<SetStateAction<Puppy["id"][]>>;
 }) {
   return (
     <ul className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {puppies.map((puppy) => (
-        <PuppyCard key={puppy.id} puppy={puppy} liked={liked} setLiked={setLiked} />
-      ))}
+      {puppies
+        .filter((pup) =>
+          pup.trait.toLowerCase().includes(searchQuery.toLowerCase()),
+        )
+        .map((puppy) => (
+          <PuppyCard
+            key={puppy.id}
+            puppy={puppy}
+            liked={liked}
+            setLiked={setLiked}
+          />
+        ))}
     </ul>
   );
 }
