99a3223239c93b7aa7186a4829547d2cd8814321
Rui Fernandes <rui95fer@gmail.com>
2025-04-28 23:29:56 +0100

    Form Actions

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Search } from "./components/Search";
 import { Shortlist } from "./components/Shortlist";
 import { PuppiesList } from "./components/PuppiesList";
 import { NewPuppyForm } from "./components/NewPuppyForm";
-import { puppies } from "./data/puppies";
+import { puppies as puppiesData } from "./data/puppies";
 import { useState } from "react";
 import { Puppy } from "./types";
 
@@ -23,6 +23,7 @@ export function App() {
 function Main() {
   const [liked, setLiked] = useState<Puppy["id"][]>([1, 3]);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [puppies, setPuppies] = useState<Puppy[]>(puppiesData);
 
   return (
     <main>
@@ -31,7 +32,7 @@ function Main() {
         <Shortlist liked={liked} setLiked={setLiked} puppies={puppies} />
       </div>
       <PuppiesList searchQuery={searchQuery} puppies={puppies} liked={liked} setLiked={setLiked} />
-      <NewPuppyForm />
+      <NewPuppyForm puppies={puppies} setPuppies={setPuppies} />
     </main>
   );
 }
